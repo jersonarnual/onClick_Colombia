2bfa6cddb9036c815a5cdb2f387b3aa33b21f032
Jerson Arnual Moreno Quevedo <jerson.moreno.out@servientrega.com>
2023-02-03 23:28:19 -0500

    Agregar archivos de proyecto.

---
+++ OnClick.Web/Controllers/AccountController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnClick.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login() => View();
+
+        public IActionResult Logout() => View();
+    }
+}
