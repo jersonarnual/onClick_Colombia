324159aa7a92816a5043c0f617c3e1388af6aaeb
Jerson Arnual Moreno Quevedo <jerson.moreno.out@servientrega.com>
2023-02-04 12:56:33 -0500

    implemtacion fron login

--- OnClick.Web/Program.cs
+++ OnClick.Web/Program.cs
@@ -51,6 +51,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Account}/{action=Login}/{id?}");
+pattern: "{controller=Account}/{action=Login}/{id?}");
+//pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
